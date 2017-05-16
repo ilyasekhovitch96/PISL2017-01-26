3bede25fda61e44e77f7f45dda09fec05db710cd
User <ilyasekhovitch96@gmail.com>
2017-05-16 22:26:32 +0300

    completed 1-2 tasks

---
+++ src/by/it/group473602/sekhovitch/lesson01/FiboA.java
@@ -0,0 +1,58 @@
+package by.it.group473602.sekhovitch.lesson01;
+
+import java.math.BigInteger;
+
+/*
+ * Вам необходимо выполнить рекурсивный способ вычисления чисел Фибоначчи
+ */
+
+public class FiboA {
+
+    private long startTime = System.currentTimeMillis();
+
+    private long time() {
+        return System.currentTimeMillis() - startTime;
+    }
+
+    public static void main(String[] args) {
+        FiboA fibo = new FiboA();
+        int n = 33;
+        System.out.printf("calc(%d)=%d \n\t time=%d \n\n", n, fibo.calc(n), fibo.time());
+        //вычисление чисел фибоначчи медленным методом (рекурсией)
+        fibo = new FiboA();
+        n = 33;
+        System.out.printf("slowA(%d)=%d \n\t time=%d \n\n", n, fibo.slowA(n), fibo.time());
+    }
+
+
+    private int calc(int n) {
+        //здесь простейший вариант, в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+        int a=1;
+        int b=1;
+        int sum_fib=0;
+        for(int i = 0; i < n-2; i++){
+            sum_fib = a + b;
+            a = b;
+            b = sum_fib;
+        }
+        return sum_fib;
+    }
+
+
+    BigInteger slowA(Integer n) {
+
+        if(n==0) return BigInteger.ZERO;
+        if(n==1) return BigInteger.ONE;
+        return (slowA(n-1).add(slowA(n-2)));
+        //рекурсия
+        //здесь нужно реализовать вариант без ограничения на размер числа,
+        //в котором код совпадает с мат.определением чисел Фибоначчи
+        //время O(2^n)
+
+        //return BigInteger.ZERO;
+    }
+
+
+
+}
